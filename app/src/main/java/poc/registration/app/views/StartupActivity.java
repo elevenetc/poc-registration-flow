5322ba06d7b120fbd92a5cb6fd4e4890d90f673b
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-26 02:39:05 +0200

    Add screens layouts

--- app/src/main/java/poc/registration/app/views/StartupActivity.java
+++ app/src/main/java/poc/registration/app/views/StartupActivity.java
@@ -10,6 +10,6 @@ public class StartupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launch);
     }
 }
